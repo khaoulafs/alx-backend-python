685a6df69224f0aed54f631b2a7e372f6fcefc27
khaoulafs <khaoulafsihi@gmail.com>
2024-03-19 23:22:30 +0000

    Tasks

---
+++ 0x02-python_async_comprehension/0-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import asyncio
+async_generator = __import__('0-async_generator').async_generator
+async def print_yielded_values():
+    result = []
+    async for i in async_generator():
+        result.append(i)
+    print(result)
+sh: 1: q: not found
